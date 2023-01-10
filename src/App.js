2c06a45d29f09d7dac340938fa4892f9d4addaf0
cyccc95 <cyccc95@naver.com>
2023-01-11 01:14:35 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import './App.css';
+import Sub from './Sub';
 
 function App() {
-  let list = [1, 2, 3];
+  // let number = 1; // 상태 값 아님
+  const [number, setNumber] = useState(1); // React 안에 hooks 라이브러리, 상태값이 됨
 
+  const add = () => {
+    setNumber(number + 1); // 리액트한테 number 값 변경할게 라고 요청
+    console.log('add : ', number);
+  };
+
+  // 랜더링 시점 = 상태값 변경
   return (
     <div>
-      {list.map((n) => (
-        <h1>{n}</h1>
-      ))}
+      <h1>숫자 : {number}</h1>
+      <button onClick={add}>더하기</button>
+      <Sub />
     </div>
   );
 }
---
+++ src/Sub.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sub = () => {
+  // 리턴을 관리할 함수 제공
+
+  return (
+    <div>
+      <h1>Sub입니다.</h1>
+    </div>
+  );
+};
+
+export default Sub;
