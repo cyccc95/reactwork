f732aa196d9e048c8220b78ed019576f0d0f5033
cyccc95 <cyccc95@naver.com>
2023-01-10 23:51:48 +0900

    배열

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-let a = 10; // 변수
-const b = 20; // 상수
-
 function App() {
-  let c; // undefined라는 값이 있는 것
-  console.log(c);
-
-  const mystyle = {
-    color: 'red',
-  };
+  let list = [1, 2, 3];
 
   return (
     <div>
-      <div style={mystyle}>
-        안녕 {a === 10 ? '10입니다.' : '10이 아닙니다.'}
-      </div>
-      <h1 className="box-style">해딩태그 {b === 20 && '20입니다.'}</h1>
-      <hr />
+      {list.map((n) => (
+        <h1>{n}</h1>
+      ))}
     </div>
   );
 }
