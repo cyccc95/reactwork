aef160859174da6a394c1cd9a65da34f5176f5b9
cyccc95 <cyccc95@naver.com>
2023-01-10 23:06:54 +0900

    기본 문법

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+let a = 10; // 변수
+const b = 20; // 상수
+
 function App() {
+  let c; // undefined라는 값이 있는 것
+  console.log(c);
+
+  const mystyle = {
+    color: 'red',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={mystyle}>
+        안녕 {a === 10 ? '10입니다.' : '10이 아닙니다.'}
+      </div>
+      <h1 className="box-style">해딩태그 {b === 20 && '20입니다.'}</h1>
+      <hr />
     </div>
   );
 }
