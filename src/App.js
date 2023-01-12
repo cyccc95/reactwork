03564481c88f2bd82f179c3159ddf19c8a94bfe0
cyccc95 <cyccc95@naver.com>
2023-01-12 23:23:00 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
+import { cleanup } from '@testing-library/react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-  console.log('App 실행됨');
+  const [data, setData] = useState(0);
 
-  const [num, setNum] = useState(5);
-
-  let sample = [
-    { id: 1, name: 'park' },
-    { id: 2, name: 'lee' },
-    { id: 3, name: 'choi' },
-    { id: 4, name: 'kim' },
-  ];
-
-  const [users, setUsers] = useState(sample);
+  const [search, setSearch] = useState(0);
 
   const download = () => {
-    // const a = sample.concat({ id: 5, name: 'Jo' });
-    setUsers([...sample, { id: num, name: 'Jo' }]);
-    setNum(num + 1);
+    // 다운로드 받고 (통신)
+    let downloadData = 5; // 가정
+    setData(downloadData);
   };
+
+  // 실행시점
+  // (1) App() 함수가 최초 실행될 때 (마운트될 때) (App() 그림이 최초 그려질 때)
+  // (2) 상태 변수가 변경될 때 (그게 dependencyList에 등록되어 있어야함)
+  // (3) 의존리스트 관리를 할 수 있다.
+  useEffect(() => {
+    console.log('useEffect 실행됨');
+    download();
+  }, [search]);
+  // [] : 어디에도 의존하지 않는다 -> 최초 한번만 실행됨
 
   return (
     <div>
-      <button onClick={download}>다운로드</button>
-      {users.map((u) => (
-        <h1>
-          {u.id},{u.name}
-        </h1>
-      ))}
+      <button
+        onClick={() => {
+          setSearch(2);
+        }}
+      >
+        검색하기
+      </button>
+      <h1>데이터 : {data}</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        더하기
+      </button>
     </div>
   );
 }
