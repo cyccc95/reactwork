aa4a6b0846461440c798e6a34b9ed5ac12eabb3b
cyccc95 <cyccc95@naver.com>
2023-01-15 21:49:50 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-import { cleanup } from '@testing-library/react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useMemo, useState } from 'react';
 
+// useMemo => 메모라이제이션(기억)
 function App() {
-  const [data, setData] = useState(0);
+  const [list, setList] = useState([1, 2, 3, 4]);
+  const [str, setStr] = useState('합계');
 
-  const [search, setSearch] = useState(0);
-
-  const download = () => {
-    // 다운로드 받고 (통신)
-    let downloadData = 5; // 가정
-    setData(downloadData);
+  const getAddResult = () => {
+    let sum = 0;
+    list.forEach((i) => (sum = sum + i)); // 한 줄이면 {} 생략 가능
+    console.log('sum 함수 실행됨 : ', sum);
+    return sum;
   };
 
-  // 실행시점
-  // (1) App() 함수가 최초 실행될 때 (마운트될 때) (App() 그림이 최초 그려질 때)
-  // (2) 상태 변수가 변경될 때 (그게 dependencyList에 등록되어 있어야함)
-  // (3) 의존리스트 관리를 할 수 있다.
-  useEffect(() => {
-    console.log('useEffect 실행됨');
-    download();
-  }, [search]);
-  // [] : 어디에도 의존하지 않는다 -> 최초 한번만 실행됨
+  const addResult = useMemo(() => getAddResult(), [list]);
 
   return (
     <div>
       <button
         onClick={() => {
-          setSearch(2);
+          setStr('안녕');
         }}
       >
-        검색하기
+        문자 변경
       </button>
-      <h1>데이터 : {data}</h1>
       <button
         onClick={() => {
-          setData(data + 1);
+          setList([...list, 10]);
         }}
       >
-        더하기
+        리스트값 추가
       </button>
+      <div>
+        {list.map((i) => (
+          <h1>{i}</h1>
+        ))}
+      </div>
+      <div>
+        {str} : {addResult}
+      </div>
     </div>
   );
 }
