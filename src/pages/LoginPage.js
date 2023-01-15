a89c489ff2a1b5fc1f52496e00d979a869043fab
cyccc95 <cyccc95@naver.com>
2023-01-16 02:19:27 +0900

    useParams, useNavigate

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import Login from '../components/login/Login';
 
 const LoginPage = () => {
+  const { id } = useParams();
+  console.log(id);
+
+  const navigate = useNavigate();
+
   return (
     <div>
+      <button
+        onClick={() => {
+          navigate('/'); // 1페이지 뒤로는 -1
+        }}
+      >
+        홈으로
+      </button>
       <Login />
     </div>
   );
