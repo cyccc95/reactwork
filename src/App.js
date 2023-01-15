082873085d311e5d3d09510c70e71233a1038091
cyccc95 <cyccc95@naver.com>
2023-01-15 22:43:50 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-// import styled from 'styled-components';
-import { Title } from './MyCss';
-
-const a = {
-  backgroundColor: 'red',
-};
-
-// const Title = styled.h1`
-//   font-size: 1.5em;
-//   text-align: center;
-//   color: palevioletred;
-// `;
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div>
-      <div style={a}>안녕</div>
-      <div className="box-style">헬로</div>
-      <Title>잘가</Title>
+      <LoginPage />
     </div>
   );
 }
